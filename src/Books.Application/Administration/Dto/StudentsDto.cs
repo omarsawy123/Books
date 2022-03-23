76579fd42f66e58347d090855cefdf3140bbe064
omar sawy <omarsawy51@gmail.com>
2022-03-23 21:51:11 +0200

    Students Services

---
+++ src/Books.Application/Administration/Dto/StudentsDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Abp.Application.Services.Dto;
+using Abp.AutoMapper;
+using Abp.Domain.Entities.Auditing;
+using Books.Administration;
+
+
+namespace Books.Administration.Dto
+{
+    [AutoMap(typeof(Students))]
+    public class StudentsDto:EntityDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ src/Books.Application/Administration/StudentsAppService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Abp.Application.Services;
+using Abp.Domain.Repositories;
+using Books.Administration;
+using Books.Administration.Dto;
+
+namespace Books.StudentsAppServices
+{
+    public  class StudentsAppService: AsyncCrudAppService<Students, StudentsDto>
+    {
+
+        public StudentsAppService(IRepository<Students> repository)
+        : base(repository)
+        {
+
+        }
+    }
+}
