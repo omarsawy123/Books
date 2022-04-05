0099618ba8381b3eee543604e265178911d06682
omar sawy <omarsawy51@gmail.com>
2022-04-05 13:07:23 +0200

    publishers

---
+++ src/Books.Application/Administration/Publishers/Dto/PublisherDto.cs
@@ -0,0 +1,17 @@
+﻿using Abp.Application.Services.Dto;
+using Abp.AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Books.Administration;
+
+namespace Books.Administration.Dto
+{
+    [AutoMap(typeof(Publishers))]
+    public class PublisherDto:EntityDto
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ src/Books.Application/Administration/Publishers/PublishersAppService.cs
@@ -0,0 +1,26 @@
+﻿using Abp.Application.Services;
+using Abp.Application.Services.Dto;
+using Abp.Domain.Repositories;
+using Books.Administration.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Books.Administration.PublisherAppService
+{
+    public class PublishersAppService : AsyncCrudAppService<Publishers,PublisherDto>
+    {
+
+        private readonly IRepository<Publishers, int> _puslishers;
+
+        public PublishersAppService(IRepository<Publishers, int> puslishers) : base(puslishers)
+        {
+            _puslishers = puslishers;
+        }
+
+       
+
+    }
+}
